bc9ed5ff501792adfab272a7688c131d34335b80
Swifty1235 <104468694+Swifty1235@users.noreply.github.com>
2024-04-03 20:24:24 -0400

    Initial Commit

---
+++ XCode Tutorial/XCode_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode_TutorialApp.swift
+//  XCode Tutorial
+//
+//  Created by Pedro Romero on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct XCode_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
